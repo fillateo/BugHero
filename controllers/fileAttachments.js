4f72dbcd37c615f226a1891b8cc53163d0eaf1d5
fillateo <jaitramadandij@gmailcom>
2021-08-14 11:49:15 +0800

    Add fileAttachments controller

---
+++ controllers/fileAttachments.js
@@ -0,0 +1,63 @@
+const fs = require('fs')
+const Project = require('../models/Project')
+const FileAttachment = require('../models/FileAttachment')
+
+module.exports = {
+  upload: async (req, res) => {
+    const project = await Project.findById(req.params.projectId)
+
+    if (project.user._id != req.user.id) {
+      return res.redirect('/projects/1')
+    }
+
+    try {
+      const file = req.file.path
+
+      if (!file) {
+        res.render('error/400', { layout: 'layouts/layoutError' })
+      }
+
+      req.body.project = project
+      req.body.user = req.user
+      req.body.file = file
+      await FileAttachment.create(req.body)
+
+      res.redirect(`/projects/details/${req.params.projectId}`)
+    } catch (error) {
+      console.log(error)
+      res.render('error/500', { layout: 'layouts/layoutError' })
+    }
+  },
+
+  download: async (req, res) => {
+    try {
+      res.download(req.query.file)
+    } catch (err) {
+      console.log(err)
+      res.render('error/500', { layout: 'layouts/layoutError' })
+    }
+  },
+
+  delete: async (req, res) => {
+    try {
+      const file = await FileAttachment.findById(req.params.id)
+        .populate('user project')
+        .lean()
+
+      if (!file) {
+        return res.render('error/404', { layout: 'layouts/layoutError' })
+      }
+
+      if (file.user._id != req.user.id) {
+        res.redirect(`/projects/details/${file.project._id}`)
+      } else {
+        await FileAttachment.remove({ _id: req.params.id })
+        fs.unlinkSync(file.file)
+        res.redirect(`/projects/details/${file.project._id}`)
+      }
+    } catch (err) {
+      console.error(err)
+      return res.render('error/500', { layout: 'layouts/layoutError' })
+    }
+  },
+}
--- routes/fileAttachments.js
+++ routes/fileAttachments.js
@@ -1,10 +1,9 @@
 const express = require('express')
-const fs = require('fs')
 const { isAuthenticated } = require('../middleware/auth')
-const Project = require('../models/Project')
-const FileAttachment = require('../models/FileAttachment')
 
 const upload = require('../middleware/upload')
+
+const controller = require('../controllers/fileAttachments')
 
 const router = express.Router()
 
@@ -14,66 +13,15 @@ router.post(
   '/:projectId/',
   isAuthenticated,
   upload.single('file'),
-  async (req, res) => {
-    const project = await Project.findById(req.params.projectId)
-
-    if (project.user._id != req.user.id) {
-      return res.redirect('/projects/1')
-    }
-
-    try {
-      const file = req.file.path
-
-      if (!file) {
-        res.render('error/400', { layout: 'layouts/layoutError' })
-      }
-
-      req.body.project = project
-      req.body.user = req.user
-      req.body.file = file
-      await FileAttachment.create(req.body)
-
-      res.redirect(`/projects/details/${req.params.projectId}`)
-    } catch (error) {
-      console.log(error)
-      res.render('error/500', { layout: 'layouts/layoutError' })
-    }
-  }
+  controller.upload
 )
 
 // @desc    Download project file attachment
 // @route   GET /issueattachments/?file=blqbla
-router.get('/', isAuthenticated, async (req, res) => {
-  try {
-    res.download(req.query.file)
-  } catch (err) {
-    console.log(err)
-    res.render('error/500', { layout: 'layouts/layoutError' })
-  }
-})
+router.get('/', isAuthenticated, controller.download)
 
 // @desc    Delete project file attachment
 // @route   DELETE /issueattachments/:id
-router.delete('/:id', isAuthenticated, async (req, res) => {
-  try {
-    const file = await FileAttachment.findById(req.params.id)
-      .populate('user project')
-      .lean()
+router.delete('/:id', isAuthenticated, controller.delete)
 
-    if (!file) {
-      return res.render('error/404', { layout: 'layouts/layoutError' })
-    }
-
-    if (file.user._id != req.user.id) {
-      res.redirect(`/projects/details/${file.project._id}`)
-    } else {
-      await FileAttachment.remove({ _id: req.params.id })
-      fs.unlinkSync(file.file)
-      res.redirect(`/projects/details/${file.project._id}`)
-    }
-  } catch (err) {
-    console.error(err)
-    return res.render('error/500', { layout: 'layouts/layoutError' })
-  }
-})
 module.exports = router
