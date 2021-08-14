1aefb158a549010e30b41b3c04b8591ed5d9e6ee
fillateo <jaitramadandij@gmailcom>
2021-08-14 10:51:49 +0800

    Add dashboard controller

---
+++ controllers/dashboard.js
@@ -0,0 +1,59 @@
+const faker = require('faker')
+const Issue = require('../models/Issue')
+const Project = require('../models/Project')
+const User = require('../models/User')
+const Comment = require('../models/Comment')
+
+module.exports = {
+  index: async (req, res) => {
+    const issuesByPriority = await Issue.aggregate([
+      { $group: { _id: '$priority', count: { $sum: 1 } } },
+    ])
+    const issuesByType = await Issue.aggregate([
+      { $group: { _id: '$type', count: { $sum: 1 } } },
+    ])
+    const issuesOpen = await Issue.find({ status: 'Open' }).count()
+    const issuesClosed = await Issue.find({ status: 'Closed' }).count()
+    const projectsCount = await Project.find({ members: req.user.id }).count()
+    const myProjects = await Project.find({ user: req.user }).count()
+
+    res.render('dashboard', {
+      issuesByPriority,
+      issuesByType,
+      projectsCount,
+      issuesOpen,
+      issuesClosed,
+      myProjects,
+    })
+  },
+
+  generateFakeProject: async (req, res, next) => {
+    for (let i = 0; i < 20; i += 1) {
+      const project = new Project()
+
+      project.user = await User.findOne()
+      project.title = faker.commerce.productName()
+      project.description = faker.commerce.productDescription()
+
+      project.save((err) => {
+        if (err) throw err
+      })
+    }
+    res.redirect('/projects/1')
+  },
+
+  generateFakeComment: async (req, res, next) => {
+    for (let i = 0; i < 20; i += 1) {
+      const comment = new Comment()
+
+      comment.user = await User.findOne()
+      comment.issue = await Issue.findById('6110fa761d20aa57bd35342e')
+      comment.comment = faker.commerce.productDescription()
+
+      comment.save((err) => {
+        if (err) throw err
+      })
+    }
+    res.redirect('/projects/1')
+  },
+}
--- routes/index.js
+++ routes/index.js
@@ -1,70 +1,21 @@
 const express = require('express')
-const faker = require('faker')
 
-const router = express.Router()
 const { isAuthenticated } = require('../middleware/auth')
 
-const Issue = require('../models/Issue')
-const Project = require('../models/Project')
-const User = require('../models/User')
-const Comment = require('../models/Comment')
+const router = express.Router()
+
+const controller = require('../controllers/dashboard')
 
 // @desc    Dashboard/Landing page
 // @route   GET /
-router.get('/', isAuthenticated, async (req, res) => {
-  const issuesByPriority = await Issue.aggregate([
-    { $group: { _id: '$priority', count: { $sum: 1 } } },
-  ])
-  const issuesByType = await Issue.aggregate([
-    { $group: { _id: '$type', count: { $sum: 1 } } },
-  ])
-  const issuesOpen = await Issue.find({ status: 'Open' }).count()
-  const issuesClosed = await Issue.find({ status: 'Closed' }).count()
-  const projectsCount = await Project.find({ members: req.user.id }).count()
-  const myProjects = await Project.find({ user: req.user }).count()
-
-  res.render('dashboard', {
-    issuesByPriority,
-    issuesByType,
-    projectsCount,
-    issuesOpen,
-    issuesClosed,
-    myProjects,
-  })
-})
+router.get('/', isAuthenticated, controller.index)
 
 /* eslint-disable no-await-in-loop */
-// fake data generate
-router.get('/generate-fake-project-data', async (req, res, next) => {
-  for (let i = 0; i < 20; i += 1) {
-    const project = new Project()
+// fake project generate
+router.get('/generate-fake-project-data', controller.generateFakeProject)
 
-    project.user = await User.findOne()
-    project.title = faker.commerce.productName()
-    project.description = faker.commerce.productDescription()
-
-    project.save((err) => {
-      if (err) throw err
-    })
-  }
-  res.redirect('/projects/1')
-})
-
-// fake data generate
-router.get('/generate-fake-comment-data', async (req, res, next) => {
-  for (let i = 0; i < 20; i += 1) {
-    const comment = new Comment()
-
-    comment.user = await User.findOne()
-    comment.issue = await Issue.findById('6110fa761d20aa57bd35342e')
-    comment.comment = faker.commerce.productDescription()
-
-    comment.save((err) => {
-      if (err) throw err
-    })
-  }
-  res.redirect('/projects/1')
-})
+// fake comment generate
+router.get('/generate-fake-comment-data', controller.generateFakeComment)
 /* eslint-enable no-await-in-loop */
 
 module.exports = router
