b074c7dc0f036d109ce038e952c383ce7b85b46d
fillateo <jaitramadandij@gmailcom>
2021-08-14 08:10:22 +0800

    remove unused middleware

--- config/auth.js
+++ config/auth.js
@@ -1,13 +1,6 @@
 const crypto = require('crypto')
 
 module.exports = {
-  ensureAuthentication: function (req, res, next) {
-    if (req.isAuthenticated()) {
-      return next()
-    }
-    req.flash('errors', { msg: 'You need to logged in' })
-    res.redirect('/login')
-  },
   generateProfileImage: function (email) {
     const md5 = crypto.createHash('md5').update(email).digest('hex')
     return `https://www.gravatar.com/avatar/${md5}?s=200&d=identicon`
--- middleware/auth.js
+++ middleware/auth.js
@@ -6,6 +6,7 @@ module.exports = {
       return next()
     }
 
+    req.flash('errors', { msg: 'You need to logged in' })
     return res.redirect('/users/login')
   },
 
