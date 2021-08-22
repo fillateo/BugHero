d8041f7c9b77cae1f108b509ed0ce4c45eb49cbd
fillateo <jaitramadandij@gmailcom>
2021-08-23 07:35:34 +0800

    Add absolute url

--- config/passport.js
+++ config/passport.js
@@ -46,7 +46,7 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback',
+        callbackURL: `${process.env.ABSOLUTE_URL}/auth/google/callback`,
       },
       async (accessToken, refreshToken, profile, done) => {
         const newUser = {
