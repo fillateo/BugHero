1b20a2fbd4a1a2b7f2fc255ffc33c354195261dc
fillateo <jaitramadandij@gmailcom>
2021-08-23 08:14:19 +0800

    Update passport

--- config/passport.js
+++ config/passport.js
@@ -46,7 +46,10 @@ module.exports = function (passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: `${process.env.ABSOLUTE_URL}/auth/google/callback`,
+        callbackURL:
+          process.env.NODE_ENV == 'development'
+            ? '/auth/google/callback'
+            : `${process.env.ABSOLUTE_URL}/auth/google/callback`,
       },
       async (accessToken, refreshToken, profile, done) => {
         const newUser = {
